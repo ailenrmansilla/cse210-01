0b5e925bfe5b93d4fc7c23d8054aa34fbc7a48c8
ailenrmansilla <man22007@byui.edu>
2022-05-12 17:06:15 -0300

    Game code finished

--- Hilo/hilo_game.py
+++ Hilo/hilo_game.py
@@ -1,9 +1,3 @@
-#Player: initialize it with 300 points. Guess if next card will be higher or lower. Can choose if keep playing or not.
-#Earns 100 if guess correctly, looses 75 if doesnt. If 0, game over
-#Card: is displayed. Represented with a number between 1 and 13.
-
-#many classes and method comments
-#game play and game over displays
 import random
 
 class Player:
@@ -13,30 +7,44 @@ class Player:
         self.playing = True
 
     def start_game(self):
-        pass
+        card = Card()
+        while self.playing == True:
+            first_number = card.number
+            print(f'----Your card is {card.number}----')
+            print(f'Time to guess!')
+            guess = input(f'Next card is higher or lower? (Enter H or L): ').upper
+            card = Card()
+            new_number = card.number
+            if new_number >= first_number:
+                if guess == 'H':
+                    print(f'You guessed correctly! The card\'s number was higher')
+                    self.points += 100
+                    print(f'You have {self.points} points')
+                else:
+                    print('Next card was ',new_number)
+                    self.points -= 75
+                    print(f'You have {self.points} points')
+            elif new_number <= first_number:
+                if guess == 'L':
+                    print(f'You guessed correctly! The card\'s number was lower')
+                    self.points += 100
+                    print(f'You have {self.points} points')
+                else:
+                    print('Next card was ',new_number)
+                    self.points -= 75
+                    print(f'You have {self.points} points')
+            if self.points > 0:
+                play_again = input('Play again? (Enter Y/N): ').upper()
+                if play_again == 'N':
+                    self.playing = False
+                    print('----Thanks for playing!----')
+            else:
+                print('-----GAME OVER-----')
+
     
 class Card:
     def __init__(self): 
         self.number = random.randint(1,13)
 
-
 player = Player()
-card = Card()
-while player.playing == True:
-    first_number = card.number
-    print(f'----Your card is {card.number}----')
-    print(f'Time to guess!')
-    guess = input('Higher or lower? (Enter H or L): ').upper
-    card = Card()
-    new_number = card.number
-    #fix the conditionals
-    if new_number >= first_number and guess == 'H':
-        print(f'You guessed correctly! The card\'s number was higher')
-    elif new_number <= first_number and guess == 'L':
-        print(f'You guessed correctly! The card\'s number was lower')
-    else:
-        print('You didnt guess correctly!')
-    play_again = input('Play again? (Enter Y/N) ')
-    if play_again == 'N':
-        player.playing = False
-    
+player.start_game()
